aa4115943126424fa0b00df5ea3f7319af5a63e9
Matvey Vasilyev <hounsou734@gmail.com>
2023-09-03 23:24:52 +0300

    Zagum Test

--- src/zagum.js
+++ src/zagum.js
@@ -1,2 +1,15 @@
-// import './kochuka.css'
+import './kochuka.css'
 console.log('hi')
+document.addEventListener('DOMContentLoaded', () => {
+  let burgerMenu = document.querySelector('.A_BurgerMenu')
+  let burgerList = document.querySelector('.C_Navigation')
+  let burgerHandler = document.querySelector('.U_BurgerHandler')
+  burgerHandler.addEventListener('click', (e) => {
+    burgerMenu.classList.toggle('is_active')
+    burgerList.classList.toggle('is_active')
+  })
+})
+document.addEventListener('keydown', () => {
+  burgerMenu.classList.toggle('is_active')
+  burgerList.classList.toggle('is_active')
+})
